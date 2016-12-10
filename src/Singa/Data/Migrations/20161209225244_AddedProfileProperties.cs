d051a0402a3104046b6ebda6a5f6febec19533a1
Pavel Nedosekov <key-pasha@ya.ru>
2016-12-10 12:53:34 +0300

    init

---
+++ src/Singa/Data/Migrations/20161209225244_AddedProfileProperties.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Singa.Data.Migrations
+{
+    public partial class AddedProfileProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Institute",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryEmail",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Institute",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SecondaryEmail",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ src/Singa/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Singa.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string SecondaryEmail { get; set; }
+        public string Surname { get; set; }
+        public string PostalCode { get; set; }
+        public string Institute { get; set; }
+        public int Address { get; set; }
+        public string Department { get; set; }
+        public string CountryCode { get; set; }        
+        private string GivenName { get; set; }
+        private string Title { get; set; }
+
+        //from article: Version is timestamp of the alert
+        private DateTime Version { get; set; }
+
+    }
+}
