5897f92f3d7b741fc875f2425f8f3e8be2cc744d
fastNick <fastnick88@gmail.com>
2016-12-10 23:24:18 +0200

    Add Constants class

---
+++ src/Singa/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Singa
+{
+    public static class Constants
+    {
+       public const string InvitationString = "Hello,\n\n {0} has requested you to join Signa. \n\n Please click the following link to sign up if you do not have an account: \n {1} \n\n Please click on following link to acknowledge if you already have an account:\n {2} \n\n {3} \n\n Thanks & Regards,\n Webmasters \n Signa";
+    }
+}
