d6da1df5e4b21e0c8d1ac04d4af8c4331ed64ca3
Pavel Nedosekov <key-pasha@ya.ru>
2016-12-10 18:19:02 +0300

    added JWT, change postgres address

---
+++ src/Singa/Models/TokenOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singa.Models
+{
+    public static class TokenOptions
+    {
+        public const string Issuer = "SingaIssuer";
+        public const string Audience = "SingaAudience";
+        public static readonly string SecretKey = "Singa_mysupersecret_secretkey!@123";
+        public static SymmetricSecurityKey SigningKey => new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+        public static TimeSpan Expires = TimeSpan.FromDays(365);
+    }
+}
