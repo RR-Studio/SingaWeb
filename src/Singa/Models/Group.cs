0e54fc58d9f723d24ec0751d3f1b1f089b246be2
Artur Teregulov <info@teregulov.com>
2016-12-10 19:17:45 +0300

    add Group class

---
+++ src/Singa/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Singa.Models
+{
+    public class Group
+    {
+        public Guid GroupId { get; set; }
+        public string Name { get; set; }
+        public List<string> GroupMessages_temp { get; set; }  // TODO replace with class
+        public List<string> Users_temp { get; set; }
+        public List<string> Owners_temp { get; set; }
+
+    }
+}
