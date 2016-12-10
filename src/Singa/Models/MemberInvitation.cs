cbd63c7283769e93f6847c4bf20b68850a2ca916
fastNick <fastnick88@gmail.com>
2016-12-10 21:25:51 +0200

    Adding MemberInvitation entity

---
+++ src/Singa/Models/MemberInvitation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singa.Models
+{
+    public class MemberInvitation
+    {
+        public DateTime Version { get; set; }
+        public string Guid { get; set; }
+        public string Email { get; set; }
+        public ApplicationUser Senderid { get; set; }
+        public int? BecameUserId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int Status { get; set; }
+        public int? TargetPageId { get; set; }
+        public InviteTypes InvitationType { get; set; }
+        public InvitationStatuses InvitationStatus { get; set; }
+    }
+
+    public enum InviteTypes { Join = 0, Friend = 1, Group = 2 }
+
+    public enum InvitationStatuses { Confirmed, Requested}
+}
